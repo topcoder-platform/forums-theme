b782463376fe0929b9f324642bee2d0672402f1a
diazz <dat.nguyen.supper@gmail.com>
2020-05-08 02:21:29 +0700

    disable debug mode
    disable debug mode

--- settings/configuration.php
+++ settings/configuration.php
@@ -10,4 +10,4 @@ $Configuration['DebugAssets'] = TRUE;
 $Configuration['Badges']['BadgesModule']['Target'] = 'AfterUserInfo';
 $Configuration['Feature']['NewFlyouts']['Enabled'] = true;
 $Configuration['Feature']['DataDrivenTitleBar']['Enabled'] = false;
-$Configuration['Debug'] = TRUE;
+// $Configuration['Debug'] = TRUE;
